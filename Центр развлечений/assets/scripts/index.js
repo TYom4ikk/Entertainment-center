5fbe996b47beef50848fbad5e0aa8538140d5941
Artyom <Zhakubekov20032006@gmail.com>
2025-03-07 10:55:54 +0500

    Creating Slider

--- Центр развлечений/assets/scripts/index.js
+++ Центр развлечений/assets/scripts/index.js
@@ -12,3 +12,41 @@ document.addEventListener('DOMContentLoaded', function() {
     var video = document.querySelector('video');
     video.muted = true;
 });
+
+// SLIDER
+const slider = document.querySelector('.slider');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+const slides = Array.from(slider.querySelectorAll('img'));
+const slideCount = slides.length;
+let slideIndex = 0;
+let isAnimating = false;
+
+function showPreviousSlide() {
+  if (isAnimating) return;
+
+  isAnimating = true;
+  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+  updateSlider();
+}
+
+function showNextSlide() {
+  if (isAnimating) return;
+
+  isAnimating = true;
+  slideIndex = (slideIndex + 1) % slideCount;
+  updateSlider();
+}
+
+function updateSlider() {
+  const translateX = -slideIndex * 100 + '%';
+  slider.style.transform = `translateX(${translateX})`;
+  setTimeout(() => {
+    isAnimating = false;
+  }, 500);
+}
+
+prevButton.addEventListener('click', showPreviousSlide);
+nextButton.addEventListener('click', showNextSlide);
+
+updateSlider();
